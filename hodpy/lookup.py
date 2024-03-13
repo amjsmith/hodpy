caedd8c7a06c3bb9ac817c41bb0386f493f6154d
amjsmith <a.m.j.smith@hotmail.co.uk>
2024-03-13 10:30:45 +0000

    Reorganised lookup directory

--- hodpy/lookup.py
+++ hodpy/lookup.py
@@ -15,7 +15,7 @@ def get_lookup_dir():
 
 def read_hod_param_file(hod_param_file):
     """
-    Read the HOD parameter file
+    Read the MXXL HOD parameter file
     """
     params = np.loadtxt(hod_param_file, skiprows=8, delimiter=",")
     Mmin_Ls, Mmin_Mt, Mmin_am          = params[0,:3]
@@ -25,26 +25,45 @@ def read_hod_param_file(hod_param_file):
     sigma_A, sigma_B, sigma_C, sigma_D = params[4,:4]
     return Mmin_Ls, Mmin_Mt, Mmin_am, M1_Ls, M1_Mt, M1_am, M0_A, M0_B, \
             alpha_A, alpha_B, alpha_C, sigma_A, sigma_B, sigma_C, sigma_D
-        
+
+
+def read_hod_param_file(hod_param_file):
+    """
+    Read the AbacusSummit HOD parameter file
+    """
+    return
 
 path = get_lookup_dir()
 
+
+######### File locations for AbacusSummit lightcone ##########
+
+
+
+
+
+
+
+
+
+######### File locations for MXXL lightcone ##########
+
 # MXXL simulation
-mxxl_mass_function = path+"/mf_fits.dat"
-mxxl_snapshots     = path+"/mxxl_snapshots.dat"
+mxxl_mass_function = path+"/mxxl/mf_fits.dat"
+mxxl_snapshots     = path+"/mxxl/mxxl_snapshots.dat"
 
 # HOD parameters for BGS mock
-bgs_hod_parameters    = path+"/hod_params.dat"
-bgs_hod_slide_factors = path+"/slide_factors.dat" # will be created if doesn't exist
+bgs_hod_parameters    = path+"/mxxl/hod_params.dat"
+bgs_hod_slide_factors = path+"/mxxl/slide_factors.dat" # will be created if doesn't exist
 
 # lookup files for central/satellite magnitudes
-central_lookup_file   = path+"/central_magnitudes.npy"   # will be created if doesn't exist
-satellite_lookup_file = path+"/satellite_magnitudes.npy" # will be created if doesn't exist
+central_lookup_file   = path+"/mxxl/central_magnitudes.npy"   # will be created if doesn't exist
+satellite_lookup_file = path+"/mxxl/satellite_magnitudes.npy" # will be created if doesn't exist
 
 # k-corrections
-kcorr_file = path+"/k_corr_rband_z01.dat"
+kcorr_file = path+"/gama/k_corr_rband_z01.dat"
 
 # SDSS/GAMA luminosity functions
-sdss_lf_tabulated = path+"/sdss_cumulative_lf.dat"
-gama_lf_fits      = path+"/lf_params.dat"
-target_lf         = path+"/target_lf.dat" # will be created if doesn't exist
+sdss_lf_tabulated = path+"/sdss/sdss_cumulative_lf.dat" # low z SDSS LF
+gama_lf_fits      = path+"/gama/lf_params.dat" # high z GAMA LF
+target_lf         = path+"/mxxl/target_lf.dat" # will be created if doesn't exist

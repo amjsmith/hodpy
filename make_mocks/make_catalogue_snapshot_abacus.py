f22aa2c9393fbe6fa4825bf3437460c4a549775b
amjsmith <amjsmith89@gmail.com>
2024-05-30 17:19:19 +0100

    Update make_catalogue_snapshot_abacus.py

--- make_mocks/make_catalogue_snapshot_abacus.py
+++ make_mocks/make_catalogue_snapshot_abacus.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 from __future__ import print_function
 import numpy as np
-from astropy.table import Table
+from astropy.table import Table, vstack
 
 import sys
 sys.path.append('..')
